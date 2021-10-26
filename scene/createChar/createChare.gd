4550e33bd98f960a360039a79b7868c2d7d0af55
ovisannn <ovi.sanjaya11@gmail.com>
2021-10-26 14:51:07 +0700

    added character scne

--- scene/createChar/createChare.gd
+++ scene/createChar/createChare.gd
@@ -1,11 +1,11 @@
 extends Node2D
 
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+var charaGender = "male"
+var charaName = ""
 
-
+func saveData():
+	print(charaName)
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -14,3 +14,12 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
+
+
+func _on_createButton_pressed():
+	
+	saveData()
+
+
+func _on_charaNameText_text_changed(new_text):
+	charaName = new_text
