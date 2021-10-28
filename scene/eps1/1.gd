34b5c28f1b53f1bd4f8dd535c2dcbc9ceb4c6109
ovisannn <ovi.sanjaya11@gmail.com>
2021-10-29 02:23:28 +0700

    more useable later fetch code

--- scene/eps1/1.gd
+++ scene/eps1/1.gd
@@ -4,18 +4,17 @@ extends Node2D
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
-
-
-# Called when the node enters the scene tree for the first time.
+const fileLocation = "res://gameData/playerStat.json"
+onready var gameLoad = $loadGameStat
+	
 func _ready():
-	print("loaded")
-	var dialog = Dialogic.start("test")
-	var a = Dialogic.get_variable("isMale")
-	print(a)
-	Dialogic.set_variable("isMale", 1)
-	print(Dialogic.get_variable("isMale"))
-	add_child(dialog)
-
+	#var person = loadGame(fileLocation)
+	#loadGameProperty(person)
+	var person = gameLoad.loadGame()
+	gameLoad.loadGameProperty(person)
+	#dialog initiate
+	var new_dialog = Dialogic.start('test')
+	add_child(new_dialog)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
