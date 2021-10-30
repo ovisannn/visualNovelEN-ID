71b70eddd1f556ef4a13792163c8531b60f604de
ovisannn <ovi.sanjaya11@gmail.com>
2021-10-30 09:49:45 +0700

    updatig progress on eps 1

--- scene/eps1/1.gd
+++ scene/eps1/1.gd
@@ -1,21 +1,15 @@
 extends Node2D
 
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-const fileLocation = "res://gameData/playerStat.json"
 onready var gameLoad = $loadGameStat
 	
 func _ready():
-	#var person = loadGame(fileLocation)
+
 	#loadGameProperty(person)
 	var person = gameLoad.loadGame()
 	gameLoad.loadGameProperty(person)
+	
 	#dialog initiate
-	var new_dialog = Dialogic.start('test')
+	var new_dialog = Dialogic.start('day1-part1')
 	add_child(new_dialog)
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-	#print(dialog.isMale)
